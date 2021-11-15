9d037076f0939dff9bf7e6b26c8af28705fd1f7f
Ksenia Baranova <baranova-ksenia@mail.ru>
2021-11-15 10:05:13 +0200

    header and navbar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./index.css";
 function App() {
   return (
     <div className="App">
-      {/*  <Header /> */}
+      <Header />
       <Home />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,115 @@
 import React from "react";
+import styled from "styled-components";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function Header() {
-  return <div>header</div>;
+  return (
+    <Container>
+      <a href="#">
+        <img src="/images/logo.svg" alt="logo" />
+      </a>
+      <Menu>
+        <a href="#">Model S</a>
+        <a href="#">Model 3</a>
+        <a href="#">Model X</a>
+        <a href="#">Model Y</a>
+      </Menu>
+      <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Tesla account</a>
+        <CustomMenu />
+      </RightMenu>
+      <BurgerNav>
+        <li>
+          <a href="#">Existing Inventory</a>
+        </li>
+        <li>
+          <a href="#">Used Inventory</a>
+        </li>
+        <li>
+          <a href="#">Trade-in</a>
+        </li>
+        <li>
+          <a href="#">Cybertruck</a>
+        </li>
+        <li>
+          <a href="#">Roadaster</a>
+        </li>
+        <li>
+          <a href="#">Existing Inventory</a>
+        </li>
+      </BurgerNav>
+    </Container>
+  );
 }
 
 export default Header;
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  padding: 0 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  z-index: 1;
+  //full width of
+  top: 0;
+  left: 0;
+  right: 0;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 1;
+  justify-content: center;
+
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 25px;
+  }
+  a:last-child {
+    margin-right: 0;
+  }
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+  }
+`;
+
+const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
+`;
+
+const BurgerNav = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 10;
+  background: #ffffff;
+  width: 300px;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+    a {
+      font-weight: 600;
+    }
+  }
+`;
